17fe6960be70c7e568428bab450e421ade833562
Дмитрий Карих <the.dr.hax@gmail.com>
2016-10-16 13:58:42 +0300

    check.php: Finish migration to Piwik

--- check.php
+++ check.php
@@ -2,8 +2,6 @@
 	// Проверка на интернет в приложении "Wi-Fi в метро" происходит путем сравнивания ответа сервера с этой строкой
 	print("2fv3bYW6x92V3Y7gM5FfT7Wmh<br>");
 
-	include 'config.php';
-
 	if (!empty($_POST['isAutomatic'])) { // Поддержка версий ниже 1.1-9
 		$automatic = $_POST['isAutomatic'];
 	} else { // Версии больше и равные 1.1-9
@@ -25,10 +23,6 @@
 
 	$connected = $_POST['connected'];
 
-//	$query = "INSERT INTO mosmetro_stat(version, automatic, connected, ssid)" .
-//		" VALUES ('" . $version . "', " . $automatic . ", " . $connected . ", '" . $ssid . "')";
-//	mysqli_query($mysqli, $query);
-
 	$data = [
 		"1" => ["version", $version],
 		"2" => ["ssid", $ssid],
@@ -38,8 +32,6 @@
 	header("Location: /piwik/piwik.php?"
 			. "idsite=6&"
 			. "rec=1&"
-			//. "token_auth=a2176dc663545fb23a95b6aa9d7e6765&"
-			//. "cip=" . $_SERVER['REMOTE_ADDR'] . "&"
 			. "url=" . urlencode("https://thedrhax.pw/mosmetro/check.php") . "&"
 			. "_cvar=" . json_encode($data)
 	);
