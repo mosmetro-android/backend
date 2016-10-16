381263a650fd05ee88b5f84b28dd0514f375e80c
Дмитрий Карих <the.dr.hax@gmail.com>
2016-10-16 23:55:09 +0300

    check.php: Remove obsolete parameters

--- check.php
+++ check.php
@@ -1,13 +1,4 @@
 <?php
-	// Проверка на интернет в приложении "Wi-Fi в метро" происходит путем сравнивания ответа сервера с этой строкой
-	print("2fv3bYW6x92V3Y7gM5FfT7Wmh<br>");
-
-	if (!empty($_POST['isAutomatic'])) { // Поддержка версий ниже 1.1-9
-		$automatic = $_POST['isAutomatic'];
-	} else { // Версии больше и равные 1.1-9
-		$automatic = $_POST['automatic'];
-	}
-
 	// Версии большие и равные 1.6-29
 	if (!empty($_POST['ssid'])) {
 		$ssid = $_POST['ssid'];
