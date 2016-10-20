29a45ad2cfc83a324944e68dea295f2dbe0af246
Дмитрий Карих <the.dr.hax@gmail.com>
2016-10-20 21:17:04 +0300

    API: v1: download: Throw exception if branch is not passed

--- api/v1/download.php
+++ api/v1/download.php
@@ -3,27 +3,31 @@
 
     include $ROOT . '/config.php';
     include $ROOT . '/lib/branches.php';
-    
-    if (!empty($_GET['branch'])) {
-        $branch = $_GET['branch'];
-        $url = $branches[$branch]['url'];
-        
-        if (empty($url)) {
-            $result = array();
-            $result['success'] = False;
-            $result['status'] = 'not found';
-            echo json_encode($result);
-            exit();
-        }
-        
-        $apk = cached_retriever($url, 0);
-        
-        header("Content-Type: application/octet-stream");
-        header("Content-Disposition: attachment; filename="
-                    . "\"MosMetro-" . $branch . "-signed.apk\"");
-        header("Content-Length: " . strlen($apk));
-        
-        echo $apk;
+
+    function fail() {
+        $result = array();
+        $result['success'] = False;
+        $result['status'] = 'branch not found';
+        echo json_encode($result);
         exit();
     }
+
+    if (empty($_GET['branch'])
+        fail();
+
+    $branch = $_GET['branch'];
+    $url = $branches[$branch]['url'];
+
+    if (empty($url))
+        fail();
+
+    $apk = cached_retriever($url, 0);
+
+    header("Content-Type: application/octet-stream");
+    header("Content-Disposition: attachment; filename="
+                . "\"MosMetro-" . $branch . "-signed.apk\"");
+    header("Content-Length: " . strlen($apk));
+
+    echo $apk;
+    exit();
 ?>
