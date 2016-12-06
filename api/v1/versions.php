135f47c2e0b90c0f114182cfe3ee564ca9c75fc0
Дмитрий Карих <the.dr.hax@gmail.com>
2016-12-06 19:13:08 +0300

    API: v1: versions.php: Pick domain from request

--- api/v1/versions.php
+++ api/v1/versions.php
@@ -1,12 +1,12 @@
 <?php
     $ROOT = __DIR__ . '/../..';
-    $DOMAIN = "http://wi-fi.metro-it.com";
+    $DOMAIN = "http://" . $_SERVER['HTTP_HOST'];
 
     include $ROOT . '/config.php';
     include $ROOT . '/lib/branches.php';
-    
+
     $result = array();
-    
+
     foreach (array_keys($branches) as $branch) {
         $branches[$branch]['message'] = nl2br($branches[$branch]['message']);
         $branches[$branch]['url'] = $DOMAIN . "/api/v1/download.php?branch="
