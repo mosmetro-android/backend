b1e47aaa9b96dc5c74e01935a6c88393073d9af8
Дмитрий Карих <the.dr.hax@gmail.com>
2017-08-28 04:33:53 +0300

    lib/flush.php: Remove empty function

--- html/lib/flush.php
+++ html/lib/flush.php
@@ -9,10 +9,6 @@ if (file_exists(__ROOT__ . "/config.php")) {
 
 require_once __ROOT__ . "/lib/cache.php";
 
-function flush_cache() {
-    
-}
-
 if ($pref_cache['flush_enabled']) {
     if ($_POST['password'] == $pref_cache['flush_password']) {
         $cache = new CacheConnection;
