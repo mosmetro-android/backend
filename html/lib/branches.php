31d20b0d28a2ed62507f19e1f0df0ad9849f2a50
Дмитрий Карих <the.dr.hax@gmail.com>
2017-10-04 22:18:17 +0300

    lib/branches: Update link to Jenkins

--- html/lib/branches.php
+++ html/lib/branches.php
@@ -27,7 +27,7 @@
                 }
             }
         }
-        
+
         $result['version'] = "" . 0;
         $result['build'] = "" . $jenkins['number'];
         $result['by_build'] = "" . 1;
@@ -127,7 +127,7 @@
         $branches = array();
 
         $branches += get_branches_from_jenkins(
-            "https://local.thedrhax.pw/jenkins/job/MosMetro-Android"
+            "https://jenkins.thedrhax.pw/job/MosMetro-Android"
         );
         $branches += get_branches_from_github(
             "mosmetro-android/mosmetro-android"
