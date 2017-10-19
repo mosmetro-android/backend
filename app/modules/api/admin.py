4e37b27ad7dfd94f77eb5d0b042cbce9baf8cd77
Дмитрий Карих <the.dr.hax@gmail.com>
2017-10-19 21:23:57 +0300

    Implement basic auth on /api/admin
    Source: http://flask.pocoo.org/snippets/8/

--- app/modules/api/admin.py
+++ app/modules/api/admin.py
@@ -4,11 +4,33 @@
 import redis
 import json
 
-from flask import Blueprint
+from functools import wraps
+from flask import Blueprint, Response, request
 admin = Blueprint('admin', __name__)
 
 
+def check_auth(username, password):
+    return username == "admin" and password == "SoSecureMuchWow"
+
+
+def authenticate():
+    return Response('Could not verify your access level for that URL.\n'
+                    'You have to login with proper credentials', 401,
+                    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 @admin.route("/flush")
+@requires_auth
 def flush():
     conn = redis.StrictRedis(host='redis')
     conn.flushall()
