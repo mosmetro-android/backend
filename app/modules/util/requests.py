21fce7bbdb1a29442f5a524096bc482f3c6a07ca
Дмитрий Карих <the.dr.hax@gmail.com>
2017-10-21 03:46:33 +0300

    Fix requests-cache wrapper

--- app/modules/util/requests.py
+++ app/modules/util/requests.py
@@ -8,12 +8,15 @@ import datetime
 import redis
 
 
-class CachedRequests():
-    def __enter__(self, ttl=30*60):
+class CachedRequests(object):
+    def __init__(self, ttl=30*60):
+        self.ttl = ttl
+
+    def __enter__(self):
         requests_cache.install_cache(
             cache_name='mosmetro',
             backend='redis',
-            expire_after=datetime.timedelta(seconds=ttl),
+            expire_after=datetime.timedelta(seconds=self.ttl),
             connection=redis.StrictRedis(host=config['redis']))
 
     def __exit__(self, type, value, traceback):
