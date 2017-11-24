11f471e9a2316dea713c8fd5911806663d661cee
Дмитрий Карих <the.dr.hax@gmail.com>
2017-11-24 21:52:25 +0300

    api/v2/stats: ban_count is not very useful

--- app/modules/api/v2.py
+++ app/modules/api/v2.py
@@ -34,6 +34,8 @@ def statistics():
     if provider == "MosMetroV2":
         increment(base + 'segment', request.form.get('segment'))
         increment(base + 'ban_bypass', request.form.get('ban_bypass'))
-        increment(base + 'ban_count', request.form.get('ban_count'))
+        increment(base + 'banned_before', "true"
+                                          if request.form.get('ban_count') > 0
+                                          else "false")
 
     return ''
